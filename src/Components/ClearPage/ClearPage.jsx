03587f746caab01410e75a8333eff66fbac045e2
R8BY <vansosnova@mail.ru>
2022-01-30 23:59:10 +0300

    Make, some shit, and delete forecast page

--- src/Components/ClearPage/ClearPage.jsx
+++ src/Components/ClearPage/ClearPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import classes from "./ClearPage.module.css"
-import ClearImg from "../../img/clearpage.png"
+import ClearImg from "../../img/clearpageimg.png"
 
 const ClearPage = () => {
     return (
