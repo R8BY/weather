cde2fef4bff9666f3d0a1fb1a749fb8b6a8d145d
SoEasy2 <soeasy24@mail.ru>
2022-01-30 20:50:00 +0300

    UNVZLOM

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+
+
+
+
 const API_key = "4f06dc23d84f10a7515fd641c25cbca3";
 const URL = "https://api.openweathermap.org/data/2.5/weather";
 export const fetchWeather = async (query) => {
