dc164ac03f2fe6f192cdff6050a9945df253a8c9
SoEasy2 <soeasy24@mail.ru>
2022-01-30 20:22:44 +0300

    VZLOM

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, {useEffect, useState} from "react";
 import './App.css';
-import Info from "./Components/items/Info";
-import CleanPage from "./Pages/CleanPage";
-import Forecast from "./Components/Forecast/Forecast";
-import Header from "./Components/Header/Header";
-import {
-    Routes,
-    Route,
-} from "react-router-dom";
+
 import {fetchWeather} from "./api/fetchWeather";
 
 function App() {
@@ -21,15 +14,7 @@ function App() {
 
     return (
         <div className={"container"}>
-            <div className={"items"}>
-                <Routes>
-                    <Route path={"/"} element={<Header setQuery={setQuery}/>}>
-                        <Route index element={<CleanPage search={search}/>}/>
-                        <Route path="weather" element={<Info weather={weather} search={search}/>}/>
-                        <Route path="forecast" element={<Forecast/>}/>
-                    </Route>
-                </Routes>
-            </div>
+
         </div>
     )
 }
