6f66ceae4bfecacf043c00cd0fad272227aeaefe
R8BY <vansosnova@mail.ru>
2022-01-30 23:54:33 +0300

    Make, some shit, and delete forecast page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React, { useState } from "react";
 import './App.css';
 import Info from "./Components/items/Info";
 import CleanPage from "./Pages/CleanPage";
-import Forecast from "./Components/Forecast/Forecast";
 import Header from "./Components/Header/Header";
 import {
     Routes,
@@ -13,6 +12,7 @@ import {fetchWeather} from "./api/fetchWeather";
 function App() {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState(null);
+
     const search = async () => {
             const data = await fetchWeather(query);
             setWeather(data);
@@ -26,7 +26,6 @@ function App() {
                     <Route path={"/"} element={<Header setQuery={setQuery}/>}>
                         <Route index element={<CleanPage search={search}/>}/>
                         <Route path="weather" element={<Info weather={weather} search={search}/>}/>
-                        <Route path="forecast" element={<Forecast/>}/>
                     </Route>
                 </Routes>
             </div>
